c8478c69a342950ba57af74bec006bee1abd4cd9
insydesw_tw\andy.chen <andy.chen@insyde.com>
2025-05-26 11:25:44 +0800

    A

--- DockerTest/7414.bat
+++ DockerTest/7414.bat
@@ -9,11 +9,13 @@ docker build -t %IMAGE_NAME%:%TAG% .
 
 if errorlevel 1 (
     echo Build ERROR!
+    pause
     exit /b 1
 )
 
 echo Build Success
-docker run --rm -it %IMAGE_NAME%:%TAG%
+REM docker run --rm -it %IMAGE_NAME%:%TAG%
+docker run -d -p 49738:80 %IMAGE_NAME%:%TAG%
 
 echo RUN END.
 pause
